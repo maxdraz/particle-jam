b4c7f51df542c4b4ee8f3191a8ccf5212a388041
Max <c16308766@mydit.ie>
2019-09-23 11:25:27 +0100

    testing mandlebrot algorithm

---
+++ Beautiful Generative Experience Unity/Assets/Scripts/Testing/MandlebrotSet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MandlebrotSet : MonoBehaviour
+{
+    public Vector3 myPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+}
