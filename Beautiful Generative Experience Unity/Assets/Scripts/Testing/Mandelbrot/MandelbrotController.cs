784f4d9f07d503f0b5da29b8aaa144c5c8ba912c
Max <c16308766@mydit.ie>
2019-09-25 13:24:40 +0100

    experimenting with golden ration point distribution

--- Beautiful Generative Experience Unity/Assets/Scripts/Testing/Mandelbrot/MandelbrotController.cs
+++ Beautiful Generative Experience Unity/Assets/Scripts/Testing/Mandelbrot/MandelbrotController.cs
@@ -4,33 +4,86 @@ using UnityEngine;
 
 public class MandelbrotController : MonoBehaviour
 {
+    public bool usingController;
     public Material mat;
     public Vector2 pos;
     public float zoomSpeed = 0.5f;
     public float scale =4;
+    private Vector2 smoothPos;
+    private float smoothScale;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        ExploreWithInput(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Input.GetAxis("Mouse ScrollWheel"));
-    }
-
-    void ExploreWithInput(float horAxis, float vertAxis, float zoomAxis)
-    {
-        float prevScale = scale;
-        scale +=-1 * zoomAxis * zoomSpeed;
-        scale = Mathf.Lerp(prevScale, scale, 1f);
-        pos += new Vector2(horAxis, vertAxis)* scale * Time.deltaTime;
-
-        mat.SetVector("_Area", new Vector4(pos.x, pos.y, scale, scale));
-    }
-
-    private void OnGUI()
-    {
-        Event e = Event.current;
-        if (e.isMouse)
+        if (usingController)
         {
-            print(e.delta);
+            HandleInputs(Input.GetAxisRaw("Horizontal"),
+                Input.GetAxisRaw("Vertical"),
+                Input.GetAxis("ZoomIn"),
+                Input.GetAxis("ZoomOut"));
+        }
+        else
+        {
+            HandleInputs(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Input.GetAxis("Jump"));
+        }
+     
+        UpdateShader();
+    }
+
+    void HandleInputs(float horAxis, float vertAxis, float zoomAxis)
+    {
+        //Movement
+        pos += new Vector2(horAxis, vertAxis) * scale * Time.deltaTime;
+
+        //Zooming
+        if (zoomAxis > 0) // scrolling up
+        {
+            scale *= .99f;
+           
+        }
+        if(zoomAxis < 0) //scrolling down
+        {
+            scale *= 1.01f* zoomSpeed;
         }
     }
+
+    void HandleInputs(float horAxis, float vertAxis, float zoomInAxis, float zoomOutAxis)
+    {
+        //Movement
+        pos += new Vector2(horAxis, vertAxis) * scale * Time.deltaTime;
+
+        //Zooming
+        if (zoomInAxis > 0) // scrolling up
+        {
+            scale *= .99f;
+
+        }
+        if (zoomOutAxis > 0) //scrolling down
+        {
+            scale *= 1.01f * zoomSpeed;
+        }
+    }
+
+    void UpdateShader()
+    {
+        smoothPos = Vector2.Lerp(smoothPos, pos, 0.03f); //makes smooth pos 0.03f closer to pos
+        smoothScale = Mathf.Lerp(smoothScale, scale, 0.03f);
+
+        float aspect = (float)Screen.width / (float)Screen.height;
+        float scaleX = smoothScale;
+        float scaleY = smoothScale;
+        if(aspect > 1) //screen is wider than its tall so scale Y proportionally less
+        {
+            scaleY /= aspect;
+        }
+        if(aspect < 1) // screen is taller than it is wider so scale X proportionally less
+        {
+            scaleX *= aspect;
+        }  
+
+        //Set the above to material properties
+        mat.SetVector("_Area", new Vector4(smoothPos.x, smoothPos.y, scaleX, scaleY));
+    }
+
+  
 }
---
+++ Beautiful Generative Experience Unity/Assets/Scripts/Testing/PointDistribution/PointDistribution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointDistribution : MonoBehaviour
+{
+    List<Vector2> points;
+    public float turnFraction = 1.61f;
+
+    private void Start()
+    {
+        points = GeneratePoints(turnFraction, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePoints(points, turnFraction);
+    }
+
+    private List<Vector2> GeneratePoints(float turnFraction, int maxIterations)
+    {
+        List<Vector2> points = new List<Vector2>();
+        Vector2 centre = Vector2.zero;        
+        float increment = (Mathf.PI * 2) * turnFraction; // 30 degrees 
+        float a =0f;
+
+        for(int i=0; i< maxIterations; i++)
+        {
+            a += increment * i;
+            Vector2 p = new Vector2(Mathf.Cos(a), Mathf.Sin(a));
+            points.Add(p);               
+        }
+
+        return points;
+    }
+
+    private void UpdatePoints(List<Vector2> p, float turnFraction)
+    {
+
+    }
+}
