7df937883cc54f3aa29697e27a74648fde7523f0
Max <c16308766@mydit.ie>
2019-09-26 09:27:56 +0100

    more work on points

---
+++ Beautiful Generative Experience Unity/Assets/Scripts/Testing/PointDistribution/ColourFromMaterial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourFromMaterial : MonoBehaviour
+{
+    private TrailRenderer trail;
+    private Material mat;
+
+    private void Start()
+    {
+        trail = GetComponent<TrailRenderer>();
+        mat = GetComponent<Renderer>().material;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            trail.startColor = mat.color;
+        }
+    }
+}
--- Beautiful Generative Experience Unity/Assets/Scripts/Testing/PointDistribution/PointDistribution.cs
+++ Beautiful Generative Experience Unity/Assets/Scripts/Testing/PointDistribution/PointDistribution.cs
@@ -4,39 +4,122 @@ using UnityEngine;
 
 public class PointDistribution : MonoBehaviour
 {
-    List<Vector2> points;
+    public int maxPoints = 5;
     public float turnFraction = 1.61f;
+    public float radius = 1f;
+    [Range(0,1)]
+    public float smoothing =0.01f;
+    public GameObject pointPrefab;
+    public List<Vector2> points;
+    public List<GameObject> objects;
 
     private void Start()
     {
-        points = GeneratePoints(turnFraction, 5);
+        points = GeneratePoints(turnFraction,radius,maxPoints);
+
+        StartCoroutine(DrawPoints(0.2f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdatePoints(points, turnFraction);
+        HandleInput(Input.GetAxis("Horizontal"));
+        UpdatePoints(ref points, turnFraction);
+        UpdateObjects(ref objects, points);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ColourObjects(ref objects);
+        }
     }
 
-    private List<Vector2> GeneratePoints(float turnFraction, int maxIterations)
+  
+
+    private List<Vector2> GeneratePoints(float turnFraction, float r, int maxIterations)
     {
         List<Vector2> points = new List<Vector2>();
-        Vector2 centre = Vector2.zero;        
+        Vector2 centre = transform.position;        
         float increment = (Mathf.PI * 2) * turnFraction; // 30 degrees 
-        float a =0f;
+        int cycles = 1;
+        float a =0f;        
 
         for(int i=0; i< maxIterations; i++)
         {
-            a += increment * i;
-            Vector2 p = new Vector2(Mathf.Cos(a), Mathf.Sin(a));
+            if(a >= Mathf.PI*2) // if completed a full turn
+            {
+                a = a % Mathf.PI * 2;
+                cycles++;
+                
+            }
+            a = increment * i;
+
+            Vector2 p;
+            
+            p = new Vector2(Mathf.Cos(a) * r*cycles, Mathf.Sin(a) * r*cycles);
+            p += centre;
+            
             points.Add(p);               
         }
 
         return points;
     }
 
-    private void UpdatePoints(List<Vector2> p, float turnFraction)
+    private void UpdatePoints(ref List<Vector2> ps, float turnFraction)
     {
+        var newpoints = GeneratePoints(turnFraction, radius, ps.Count);
+        ps = newpoints;
+    }
 
+    private void UpdateObjects(ref List<GameObject> objects, List<Vector2> points)
+    {
+       for(int i =0; i< objects.Count; i++)
+        {
+            var currentObj = objects[i];
+            var targetPos = points[i];
+            var myPos = currentObj.transform.position;
+
+            currentObj.transform.position = Vector2.Lerp(myPos, targetPos, smoothing);
+        }
+    }   
+
+    private IEnumerator DrawPoints(float t)
+    {
+        
+        //draw spheres at each point
+        foreach (Vector2 p in points)
+        {
+            var point = new Vector2(p.x, p.y);
+            //create sphere
+            var obj = Instantiate(pointPrefab,transform.position,transform.rotation);
+            obj.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            obj.transform.position = point;            
+            objects.Add(obj);
+            
+            yield return new WaitForSeconds(t);
+        }
+      
+    }
+
+    private void ColourObjects(ref List<GameObject>objs)
+    {
+        for(int i=0; i< objs.Count; i++)
+        {
+            var mat = objs[i].GetComponent<Renderer>().material;
+            float h = (float)i / ((float)objs.Count - 1);
+            mat.color = Color.HSVToRGB(h, 1, 1);
+        }
+    }
+
+    private void HandleInput(float turnFractionAxis)
+    {
+        if (turnFractionAxis > 0)
+        {
+            turnFraction *= 1.00001f;
+        }
+
+        if (turnFractionAxis <0)
+        {
+            turnFraction *= 0.99999f;
+        }
     }
 }
